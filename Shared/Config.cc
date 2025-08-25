a01d0189028f34f56821b3538c7f3fa9a99c9254
huhufat <huhu@gardn.pro>
2025-08-24 22:38:51 -0400

    config

--- Shared/Config.cc
+++ Shared/Config.cc
@@ -3,7 +3,7 @@
 #define STR1(arg) #arg
 #define ARG_TO_STR(arg) STR1(arg)
 
-extern const uint64_t VERSION_HASH = ; 
+extern const uint64_t VERSION_HASH = 0x1A2B3C4D5E6F7890ULL; 
 extern const uint32_t SERVER_PORT = 9001;
 extern const uint32_t MAX_NAME_LENGTH = 16;
 extern std::string const WS_URL = "wss://gardn.pro/";
