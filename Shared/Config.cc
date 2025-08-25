fa13925bf4bf8b14c863954dfa9cf000d4f53d4f
34130139215 <34130139215@fatherless.dad>
2025-08-24 18:54:25 -0700

    Update Config.cc

--- Shared/Config.cc
+++ Shared/Config.cc
@@ -1,9 +1,11 @@
 #include <Shared/Config.hh>
 
-extern const uint64_t VERSION_HASH = 19235684321324ull;
+#define STR1(arg) #arg
+#define ARG_TO_STR(arg) STR1(arg)
 
+extern const uint64_t VERSION_HASH = ; 
 extern const uint32_t SERVER_PORT = 9001;
 extern const uint32_t MAX_NAME_LENGTH = 16;
-
-//your ws host url may not follow this format, change it to fit your needs
-extern std::string const WS_URL = "ws://localhost:"+std::to_string(SERVER_PORT);
+extern std::string const WS_URL = "wss://gardn.pro/";
+// git clone https://github.com/emscripten-core/emsdk.git /workspaces/gardn/emsdk && cd /workspaces/gardn/emsdk && ./emsdk install latest && ./emsdk activate latest && source /workspaces/gardn/emsdk/emsdk_env.sh
+// cd emsdk && ./emsdk activate latest && source /workspaces/gardn/emsdk/emsdk_env.sh
